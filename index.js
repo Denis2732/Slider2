fa1c769e30997f0981caf2bbe077d68d40071d93
Denis2732 <plotnikovdo@yandex.ru>
2020-01-09 18:16:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+//Получаем элемент
+var backBtn = document.querySelector('.back-slide');
+var nextBtn = document.querySelector('.next-slide');
+var imgSlide = document.querySelector('img');
+
+//Добавляем слушателя
+backBtn.addEventListener('click', onBackBtn);
+nextBtn.addEventListener('click', onNextBtn);
+
+//Создаем массив картинок
+var imgUrls = [];
+imgUrls.push('img/s1.png');
+imgUrls.push('img/s2.png');
+imgUrls.push('img/s3.png');
+imgUrls.push('img/s4.png');
+imgUrls.push('img/s5.png');
+
+
+var currentImgIndex = 0;
+
+imgSlide.src = imgUrls[currentImgIndex];
+backBtn.disabled = true;
+
+function onBackBtn() {
+	currentImgIndex--;
+	nextBtn.disabled = false;
+	/*if(currentImgIndex <= 0){
+		currentImgIndex = 0;
+	}*/
+
+	if(currentImgIndex === 0){
+		backBtn.disabled = true;
+	}
+	imgSlide.src = imgUrls[currentImgIndex];
+}
+
+function onNextBtn() {
+	currentImgIndex++;
+
+	/*if(currentImgIndex >= 4){
+		currentImgIndex = 4;
+	}*/
+
+	backBtn.disabled = false;
+	if(currentImgIndex === (imgUrls.length - 1) ){
+		nextBtn.disabled = true;
+	}
+	imgSlide.src = imgUrls[currentImgIndex];
+}
